de7463145fcc263e2bc4b3b4cae3900b76219ffa
Nick Kuhn <nicklz22@yahoo.com>
2017-07-14 23:58:03 -0700

    fix

--- data/www/scripts/setup.sh
+++ data/www/scripts/setup.sh
@@ -89,7 +89,7 @@ echo "drop database clarku;" | mysql -uroot -h mysql
 echo "create database clarku;" | mysql -uroot -h mysql
 
 
-cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush @clarku.prod sql-dump > /var/www/dump.sql
+cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush @clarku.dev sql-dump > /var/www/dump.sql
 
 mysql -u root -h mysql -p clarku --password="" < /var/www/dump.sql
 
--- data/www/scripts/sync.sh
+++ data/www/scripts/sync.sh
@@ -10,8 +10,15 @@ cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush @clarku.dev s
 
 mysql -u root -h mysql -p clarku --password="" < /var/www/dump.sql
 
+cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush sqlq "create table field_data_field_show_header_bg (entity_type varchar(255), entity_id int, language varchar(255), deleted int, delta int);"
+
+cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush @none dl registry_rebuild-7.x-2.3 && drush rr
+
+cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush upwd --password="admin" "kwall"
+
+
+
 cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush cc all && drush en dblog -y
-cd /var/www/project/clarku/docroot/sites/local.clarku.edu && drush en clarku_updates -y
 
 
 
